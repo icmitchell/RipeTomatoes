c8e1758e7a91bb4ed8a948a604a3e2643df224f5
Chetan Audipudy <chetan.audipudy@gmail.com>
2018-05-17 20:09:44 -0500

    landing page

--- material-dashboard-react-html-v1.2.0/src/index.js
+++ material-dashboard-react-html-v1.2.0/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
+import LandingPage from "../src/views/LandingPage/LandingPage";
 
 import "assets/css/material-dashboard-react.css?v=1.2.0";
 
@@ -12,6 +13,7 @@ const hist = createBrowserHistory();
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
+      <Route exact path="/" component={LandingPage} />
       {indexRoutes.map((prop, key) => {
         return <Route path={prop.path} component={prop.component} key={key} />;
       })}
--- material-dashboard-react-html-v1.2.0/src/routes/index.jsx
+++ material-dashboard-react-html-v1.2.0/src/routes/index.jsx
@@ -1,4 +1,5 @@
 import Dashboard from "layouts/Dashboard/Dashboard.jsx";
+import LandingPage from "../views/LandingPage/LandingPage";
 
 const indexRoutes = [{ path: "/", component: Dashboard }];
 
